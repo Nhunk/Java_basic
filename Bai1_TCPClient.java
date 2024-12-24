0dbd117e1082f995c9647fa1d9058a85f69c10b0
Nhunk <anhnhung.edu@gmail.com>
2024-12-24 18:04:19 +0700

    ontap1

---
+++ Bai1_TCPClient.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class Bai1_TCPClient {
+
+    public static void main(String args[]) {
+        String hostname = "localhost";
+        int port = 2004;
+        try (Socket socket = new Socket(hostname, port);
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                Scanner scan = new Scanner(System.in)) {
+            String text;
+            while (true) {
+                System.out.println("Nhap so nguyen n hoac 'stop' de ket thuc: ");
+                text = scan.nextLine();
+                out.println(text);
+
+                if (text.equalsIgnoreCase("stop")) {
+                    break;
+                }
+
+                String result = in.readLine();
+                System.out.println("Ket qua: " + result);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
