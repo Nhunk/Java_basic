a67e322e889bd96904fb313cb33ab39ce6af4655
Nhunk <anhnhung.edu@gmail.com>
2024-12-29 19:31:57 +0700

    fix exam

--- Ontap/R3_RMIC_Chat.java
+++ Ontap/R3_RMIC_Chat.java
@@ -1,23 +1,40 @@
 package Ontap;
 
+import java.rmi.Naming;
+import java.util.Scanner;
+
 public class R3_RMIC_Chat {
     public static void main(String[] args) {
         try {
-            R3_RMI_interface chat = (R3_RMI_interface) java.rmi.Naming.lookup("rmi://localhost:12345/chat");
-            java.util.Scanner sc = new java.util.Scanner(System.in);
+            R3_RMI_interface chat = (R3_RMI_interface) Naming.lookup("rmi://localhost:12345/chat");
+            Scanner sc = new Scanner(System.in);
             String clientMessage, serverMessage;
+
+            // Thread to receive messages from the server
+            Thread receiveThread = new Thread(() -> {
+                try {
+                    while (true) {
+                        serverMessage = chat.receiveMessage();
+                        System.out.println("Server: " + serverMessage);
+                        if (serverMessage.equalsIgnoreCase("bye")) {
+                            System.out.println("Server disconnected.");
+                            break;
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+
+            receiveThread.start();
+
+            // Main loop to send messages to the server
             while (true) {
                 System.out.print("You: ");
                 clientMessage = sc.nextLine();
                 chat.sendMessage(clientMessage);
                 if (clientMessage.equalsIgnoreCase("bye")) {
                     System.out.println("Ending chat.");
-                    break;
-                }
-                serverMessage = chat.receiveMessage();
-                System.out.println("Client: " + serverMessage);
-                if (serverMessage.equalsIgnoreCase("bye")) {
-                    System.out.println("Client disconnected.");
                     break;
                 }
             }
--- Ontap/R3_RMIS_chat.java
+++ Ontap/R3_RMIS_chat.java
@@ -1,37 +1,52 @@
 package Ontap;
 
+import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.Scanner;
 
-public class R3_RMIS_chat extends UnicastRemoteObject implements R3_RMI_interface {
-    private static final long serialVersionUID = 1L;
+public class R3_RMIS_Chat extends UnicastRemoteObject implements R3_RMI_interface {
     private String message = "";
 
-    protected R3_RMIS_chat() throws RemoteException {
+    protected R3_RMIS_Chat() throws RemoteException {
         super();
     }
 
     @Override
-    public String sendMessage(String msg) throws RemoteException {
-        message = msg;
-        System.out.println("Client: " + message);
-        return message;
+    public synchronized void sendMessage(String message) throws RemoteException {
+        this.message = message;
+        notifyAll();
     }
 
     @Override
-    public String receiveMessage() throws RemoteException {
+    public synchronized String receiveMessage() throws RemoteException {
+        try {
+            wait();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return message;
     }
+
     public static void main(String[] args) {
         try {
-            R3_RMIS_chat chat = new R3_RMIS_chat();
-            java.rmi.registry.LocateRegistry.createRegistry(12345);
-            java.rmi.registry.Registry registry = java.rmi.registry.LocateRegistry.getRegistry(12345);
-            registry.rebind("chat", chat);
+            R3_RMIS_Chat server = new R3_RMIS_Chat();
+            Naming.rebind("rmi://localhost:12345/chat", server);
             System.out.println("Server is running...");
+
+            Scanner sc = new Scanner(System.in);
+            String serverMessage;
+            while (true) {
+                System.out.print("You: ");
+                serverMessage = sc.nextLine();
+                server.sendMessage(serverMessage);
+                if (serverMessage.equalsIgnoreCase("bye")) {
+                    System.out.println("Ending chat.");
+                    break;
+                }
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    
 }
